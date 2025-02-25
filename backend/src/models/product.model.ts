4a34e96f6c7b2fc6a97edd640318929c334c5d46
Vesa Lappalainen <144115195+vesalappari@users.noreply.github.com>
2025-02-25 12:07:12 +0200

    product model

---
+++ backend/src/models/product.model.ts
@@ -0,0 +1,10 @@
+export interface ProductModel {
+  id_number: number;
+  owner_id: number;
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  status: string;
+  image_url: string;
+}
